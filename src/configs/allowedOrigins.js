a9552ad6d4fd9fed94f0537ecb43cf98fa03afe3
truong <dodantruong333@gmail.com>
2025-10-17 21:22:10 +0700

    Split ALLOWED_ORIGINS into an array

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -1,6 +1,6 @@
 // Use environment variable for production, fallback to localhost for dev
 const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
   : ["http://localhost:5173", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
