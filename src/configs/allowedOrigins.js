17df7c60c252eb24ddb02c031ec799bf0369cd97
truong <dodantruong333@gmail.com>
2025-10-17 21:58:15 +0700

    Fix allowedOrigins

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -1,4 +1,4 @@
 // Use environment variable for production, fallback to localhost for dev
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", 'https://merry-heliotrope-1ee7ba.netlify.app/'];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000", "https://merry-heliotrope-1ee7ba.netlify.app"];
 
 module.exports = allowedOrigins;
