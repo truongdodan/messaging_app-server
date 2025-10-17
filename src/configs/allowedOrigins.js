89abbdbee176759b9d66f1b7d52e707621c8115f
truong <dodantruong333@gmail.com>
2025-10-17 21:12:50 +0700

    Update allowedOrigins.js

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -1,8 +1,6 @@
 // Use environment variable for production, fallback to localhost for dev
 const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(",")
+  ? process.env.ALLOWED_ORIGINS
   : ["http://localhost:5173", "http://localhost:3000"];
-
-module.exports = allowedOrigins;
 
 module.exports = allowedOrigins;
