5b66fd90e0a6633616a7a835acec23104d178664
dantRuongdo <dodantruong333@gmail.com>
2025-10-18 09:50:55 +0700

    Update credentials allowing method

--- src/middlewares/credentials.js
+++ src/middlewares/credentials.js
@@ -3,8 +3,12 @@ const allowedOrigins = require("../configs/allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
+  console.log("Origin: ", origin);
+  console.log("Allowed Origin: ", allowedOrigins);
+
+  if (!origin || allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Origin", origin);
   }
   next();
 };
